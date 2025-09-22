419b7f953092ba673800a3daecafab0b4a4949c0
Mohamed Khaled <amer31824@gmail.com>
2025-09-22 22:32:38 +0300

    Add project files.

---
+++ Route.MVCAPP.DAL/Persistence/Data/Configurations/Departments/DepartmentConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Route.MVCAPP.DAL.Models.Departments;
+
+namespace Route.MVCAPP.DAL.Persistence.Data.Configurations.Departments
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10,10);
+            builder.Property(D=> D.Name).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(D => D.Code).HasColumnType("varchar(50)").IsRequired();
+            builder.Property(D => D.CreatedOn).HasDefaultValueSql("GETUTCDATE()");
+            builder.Property(D => D.LastModifiedOn).HasComputedColumnSql("GETDATE()");
+        }
+    }
+}
---
+++ Route.MVCAPP.DAL/Persistence/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Route.MVCAPP.DAL.Models.Departments;
+
+namespace Route.MVCAPP.DAL.Persistence.Data.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= .;Database=MVCAPP;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
