9a03bba331e4b7d01844b05ba1fe795dbbfef3e4
Mohamed Khaled <amer31824@gmail.com>
2025-09-23 13:02:50 +0300

    Part 5 Department Repository - DAL

---
+++ Route.MVCAPP.DAL/Persistence/Repositories/Departments/DepartmentRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Route.MVCAPP.DAL.Models.Departments;
+using Route.MVCAPP.DAL.Persistence.Data.Contexts;
+
+namespace Route.MVCAPP.DAL.Persistence.Repositories.Departments
+{
+    #region Part 5 Department Repository - DAL
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public DepartmentRepository(ApplicationDbContext dbContext)
+        { // Dependency Injection -> Make CLR make Object from ApplicationDbContext and Inject it here
+
+            _dbContext = dbContext;
+        }
+        public Department? GetById(int id)
+        {
+            return _dbContext.Find<Department>(id);
+
+        }
+        public IEnumerable<Department> GetAll(bool withNoTracking = true)
+        {
+            if (withNoTracking)
+            {
+                return _dbContext.Departments.AsNoTracking().ToList(); // Detached
+            }
+            else
+            {
+                return _dbContext.Departments.ToList();   // unchanged
+            }
+        }
+
+
+        public int Add(Department entity)
+        {
+            _dbContext.Departments.Add(entity);
+            return _dbContext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbContext.Departments.Remove(entity);
+            return _dbContext.SaveChanges();
+        }
+
+
+
+
+        public int Update(Department entity)
+        {
+            _dbContext.Departments.Update(entity);
+            return _dbContext.SaveChanges();
+        }
+    } 
+    #endregion
+}
---
+++ Route.MVCAPP.DAL/Persistence/Repositories/Departments/IDepartmentRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Route.MVCAPP.DAL.Models.Departments;
+
+namespace Route.MVCAPP.DAL.Persistence.Repositories.Departments
+{
+    #region Part 5 Department Repository - DAL
+    public interface IDepartmentRepository
+    {
+        // 5 CRUD Operations
+        // 1. Get by id
+        Department? GetById(int id);
+        // 2. Get all
+        IEnumerable<Department> GetAll(bool withNoTracking = true);
+        // 3. Add
+        int Add(Department entity);
+        // 4. Update
+        int Update(Department entity);
+        // 5. Delete
+        int Delete(Department entity);
+    } 
+    #endregion
+}
